5de251adea0bd837b984fb10c84c9b736f8fed24
Sean Wu <seanwu@google.com>
2016-10-03 22:56:45 -0700

    Finish todo items

---
+++ fewd/todo/js/script.js
@@ -0,0 +1,33 @@
+var todoItem = document.querySelector('.todo-item');
+var form = document.querySelector('form');
+var newTodoItemInput = document.querySelector('.new-todo-input');
+var todoItems = document.querySelector('.todo-items');
+var noTodos = document.querySelector('.no-todos');
+var count = document.querySelector('.count');
+
+function addTodo(event) {
+  event.preventDefault();
+  var newTodo = newTodoItemInput.value;
+  if (newTodo.length > 0) {
+    noTodos.classList.add("hidden");
+    createTodo(newTodo);
+    form.reset();
+  }
+}
+
+function createTodo(todoItem) {
+  var li = document.createElement('li');
+  li.classList.add('todo-item');
+  var label = document.createElement('label');
+  var inputCheckbox = document.createElement('input');
+  inputCheckbox.setAttribute('type', 'checkbox');
+  var span = document.createElement('span');
+  span.textContent = todoItem;
+  label.appendChild(inputCheckbox);
+  label.appendChild(span);
+  li.appendChild(label);
+  todoItems.appendChild(li);
+  count.textContent = parseInt(count.textContent) + 1;
+}
+
+form.addEventListener("submit", addTodo);
